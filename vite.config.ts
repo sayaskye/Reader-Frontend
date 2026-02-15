d7f914e77042e9d33dafeb3fd5368e87466d3bb8
Andres Cazares <chaotears@gmail.com>
2026-02-14 17:13:07 -0700

    feat: imports with @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
-import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,10 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '#': path.resolve(__dirname, './public'),
+    },
+  },
 });
