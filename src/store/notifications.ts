e95eb3f640863114aad3ce918a17186e95b455d1
Andres Cazares <chaotears@gmail.com>
2026-03-01 16:46:56 -0700

    toaster

--- src/store/notifications.ts
+++ src/store/notifications.ts
@@ -13,10 +13,10 @@ interface NotificationState {
 export const useNotificationStore = create<NotificationState>((set) => ({
   criticalError: null,
 
-  notify: (message, type = 'info') => {
-    if (type === 'success') toast.success(message);
-    else if (type === 'error') toast.error(message);
-    else toast(message);
+  notify: (message, type = 'info', duration = 4000) => {
+    if (type === 'success') toast.success(message, { duration });
+    else if (type === 'error') toast.error(message, { duration });
+    else toast(message, { duration });
   },
 
   setCriticalError: (error) => set({ criticalError: error }),
