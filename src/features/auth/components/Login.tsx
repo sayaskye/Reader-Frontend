badfc296e0983d404f1ed8142cf46fb3b5ca13af
Andres Cazares <chaotears@gmail.com>
2026-03-03 00:33:44 -0700

    fix: typescript compilation error

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,4 +1,4 @@
-import LoginForm from './LogInForm';
+import LoginForm from './LoginForm';
 import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
--- src/features/auth/components/index.ts
+++ src/features/auth/components/index.ts
@@ -1,6 +1,6 @@
 export * from './AsideSection';
 export * from './AuthGuard';
-export * from './LogInForm';
 export * from './Login';
+export * from './LoginForm';
 export * from './Register';
 export * from './RegisterForm';
